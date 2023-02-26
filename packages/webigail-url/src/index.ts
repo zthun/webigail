909689129bbc7a47c593b7b01548039657ff4cd8
Anthony Bonta <zthun.coding@gmail.com>
2023-02-26 00:48:15 -0600

    feat: parse a url to retrieve domain, path, and other information from it

---
+++ packages/webigail-url/src/index.ts
@@ -0,0 +1,6 @@
+export * from './data/data-url';
+export * from './mime/mime-type';
+export * from './mime/mime-type-application';
+export * from './mime/mime-type-image';
+export * from './mime/mime-type-text';
+export * from './url/url';
