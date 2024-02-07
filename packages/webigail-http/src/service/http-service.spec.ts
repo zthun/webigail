53f92ef23d720e35b27c1c192ba91235e80550ba
Anthony Bonta <zthun.coding@gmail.com>
2024-02-07 12:34:18 -0600

    test: refactor tests to use msw

--- packages/webigail-http/src/service/http-service.spec.ts
+++ packages/webigail-http/src/service/http-service.spec.ts
@@ -1,105 +1,175 @@
-import axios, { AxiosError, AxiosHeaders } from 'axios';
-import { beforeEach, describe, expect, it, vi } from 'vitest';
-import { IZHttpRequest, ZHttpRequestBuilder } from '../request/http-request.mjs';
+import { HttpResponse, http } from 'msw';
+import { SetupServer, setupServer } from 'msw/node';
+import { afterAll, afterEach, beforeAll, describe, expect, it } from 'vitest';
+import { ZHttpRequestBuilder } from '../request/http-request.mjs';
+import { ZHttpCodeClient } from '../result/http-code-client.mjs';
+import { ZHttpCodeRedirection } from '../result/http-code-redirection.mjs';
+import { ZHttpCodeSuccess } from '../result/http-code-success.mjs';
 import { ZHttpService } from './http-service.mjs';
 
-vi.mock('axios');
-
 describe('ZHttpService', () => {
+  const Domain = 'https://mock.zthunworks.com';
+  const SuccessJson = { message: 'Success' };
+  const FailureJson = { message: 'This is Fine!' };
+  let _server: SetupServer;
+
   function createTestTarget() {
     return new ZHttpService();
   }
 
+  beforeAll(async () => {
+    _server = setupServer(
+      http.get(`${Domain}/api/success/json`, () => HttpResponse.json(SuccessJson, { status: ZHttpCodeSuccess.OK })),
+      http.post(`${Domain}/api/success/json`, async (r) =>
+        HttpResponse.json(await r.request.json(), { status: ZHttpCodeSuccess.Created })
+      ),
+      http.patch(`${Domain}/api/success/json`, async (r) =>
+        HttpResponse.json(await r.request.json(), { status: ZHttpCodeSuccess.OK })
+      ),
+      http.put(`${Domain}/api/success/json`, async (r) =>
+        HttpResponse.json(await r.request.json(), { status: ZHttpCodeSuccess.OK })
+      ),
+      http.delete(
+        `${Domain}/api/success/json`,
+        async () => new HttpResponse(null, { status: ZHttpCodeSuccess.NoContent })
+      ),
+      http.get(`${Domain}/api/failure/client`, () =>
+        HttpResponse.json(FailureJson, { status: ZHttpCodeClient.NotFound })
+      ),
+      http.get(`${Domain}/api/redirect/temporary`, () =>
+        HttpResponse.redirect(`${Domain}/api/success/json`, ZHttpCodeRedirection.TemporaryRedirect)
+      ),
+      http.get(`${Domain}/api/failure/internal`, () => {
+        throw new Error(FailureJson.message);
+      })
+    );
+    _server.listen();
+  });
+
+  afterEach(() => {
+    _server.resetHandlers();
+  });
+
+  afterAll(() => {
+    _server.close();
+  });
+
   describe('Success', () => {
-    it('should return a resolved result from the request.', async () => {
+    const url = `${Domain}/api/success/json`;
+
+    it('should return a resolved result from a GET request.', async () => {
       // Arrange
-      const expected = 'Success';
-      vi.mocked(axios).mockResolvedValue({
-        status: 200,
-        statusText: 'Success',
-        data: expected,
-        config: {},
-        headers: {}
-      });
       const target = createTestTarget();
-      const req = new ZHttpRequestBuilder().get().url('https://www.zthunworks.com/api/health').build();
+      const req = new ZHttpRequestBuilder().get().url(url).build();
       // Act
       const actual = await target.request(req);
       // Assert
-      expect(actual.data).toEqual(expected);
+      expect(actual.status).toEqual(200);
+      expect(actual.data).toEqual(SuccessJson);
+    });
+
+    it('should return a resolved result from a POST request.', async () => {
+      // Arrange.
+      const target = createTestTarget();
+      const req = new ZHttpRequestBuilder().post(SuccessJson).url(url).build();
+      // Act.
+      const actual = await target.request(req);
+      // Assert.
+      expect(actual.status).toEqual(201);
+      expect(actual.data).toEqual(SuccessJson);
+    });
+
+    it('should return a resolved result from a PATCH request.', async () => {
+      // Arrange.
+      const target = createTestTarget();
+      const req = new ZHttpRequestBuilder().patch(SuccessJson).url(url).build();
+      // Act.
+      const actual = await target.request(req);
+      // Assert.
+      expect(actual.status).toEqual(200);
+      expect(actual.data).toEqual(SuccessJson);
+    });
+
+    it('should return a resolved result from a PUT request.', async () => {
+      // Arrange.
+      const target = createTestTarget();
+      const req = new ZHttpRequestBuilder().put(SuccessJson).url(url).build();
+      // Act.
+      const actual = await target.request(req);
+      // Assert.
+      expect(actual.status).toEqual(200);
+      expect(actual.data).toEqual(SuccessJson);
+    });
+
+    it('should return a resolved result from a DELETE request.', async () => {
+      // Arrange.
+      const target = createTestTarget();
+      const req = new ZHttpRequestBuilder().delete().url(url).build();
+      // Act.
+      const actual = await target.request(req);
+      // Assert.
+      expect(actual.status).toEqual(204);
     });
   });
 
   describe('Error', () => {
-    let error: AxiosError;
-    let data: string;
-    let message: string;
-    let req: IZHttpRequest;
-
-    beforeEach(() => {
-      req = new ZHttpRequestBuilder().delete().url('https://www.zthunworks.com/api/').build();
-
-      data = 'Error data';
-      message = 'Failed';
-
-      error = {
-        name: 'Error',
-        response: {
-          data,
-          status: 404,
-          statusText: 'Not Found',
-          headers: {},
-          config: {
-            headers: new AxiosHeaders()
-          }
-        },
-        request: {},
-        message,
-        isAxiosError: true,
-        toJSON: vi.fn()
-      };
-    });
-
-    it('should return a rejected promise on failure.', async () => {
+    it('should return a rejected promise on client failure.', async () => {
       // Arrange
-      vi.mocked(axios).mockRejectedValue(error);
+      const req = new ZHttpRequestBuilder().get().url(`${Domain}/api/failure/client`).build();
       const target = createTestTarget();
       // Act
-      const actual = await target.request(req).catch((err) => err.data);
+      const actual = await target.request(req).catch((err) => err);
       // Assert
-      expect(actual).toEqual(data);
+      expect(actual.status).toEqual(404);
+      expect(actual.data).toEqual(FailureJson);
     });
 
     it('should return a rejected promise if the request was made but the endpoint cannot be hit.', async () => {
       // Arrange
-      delete error.response;
-      vi.mocked(axios).mockRejectedValue(error);
+      const req = new ZHttpRequestBuilder().get().url('https://not-an-endpoint.org').build();
       const target = createTestTarget();
       // Act
-      const actual = await target.request(req).catch((err) => err.data);
+      const actual = await target.request(req).catch((err) => err);
       // Assert
-      expect(actual).toContain('endpoint could not be reached');
+      expect(actual.status).toBeGreaterThanOrEqual(404);
+      expect(actual.data).toBeDefined();
     });
 
-    it('should return a rejected promise if the request cannot be made at all.', async () => {
+    it('should return a rejected promise if the request is not a valid url.', async () => {
       // Arrange
-      delete error.request;
-      delete error.response;
-      vi.mocked(axios).mockRejectedValue(error);
+      const req = new ZHttpRequestBuilder().get().url('lol-wut').build();
       const target = createTestTarget();
       // Act
-      const actual = await target.request(req).catch((err) => err.data);
+      const actual = await target.request(req).catch((err) => err);
       // Assert
-      expect(actual).toEqual(message);
+      expect(actual.status).toBeGreaterThanOrEqual(404);
+      expect(actual.data).toBeDefined();
     });
 
-    it('should return a rejected promise with a generic message if something else goes wrong.', async () => {
-      vi.mocked(axios).mockRejectedValue({});
+    it('should return a rejected promise if an internal exception occurs', async () => {
+      // Arrange
+      const req = new ZHttpRequestBuilder().get().url(`${Domain}/api/failure/internal`).build();
       const target = createTestTarget();
       // Act
-      const actual = await target.request(req).catch((err) => err.data);
+      const actual = await target.request(req).catch((err) => err);
       // Assert
-      expect(actual).toBeTruthy();
+      expect(actual.status).toBeGreaterThanOrEqual(500);
+      expect(actual.data).toEqual(FailureJson.message);
     });
   });
+
+  /*
+  describe('Redirect', () => {
+    it('should follow a redirect to the new URL.', async () => {
+      // Arrange.
+      const req = new ZHttpRequestBuilder().get().url(`${Domain}/api/redirect/temporary`).build();
+      const target = createTestTarget();
+      // Act.
+      const actual = await target.request(req);
+      // Assert.
+      expect(actual.status).toEqual(200);
+      expect(actual.data).toEqual(SuccessJson);
+    });
+  });
+  */
 });
