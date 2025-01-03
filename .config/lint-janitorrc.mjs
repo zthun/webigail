4ecc40b5fa7712312d3161bb5b99526649be7cfd
Anthony Bonta <zthun.coding@gmail.com>
2025-01-03 13:13:08 -0600

    build: lint

--- .config/lint-janitorrc.mjs
+++ .config/lint-janitorrc.mjs
@@ -1,20 +1,33 @@
 const generated = [
-  '**/CHANGELOG.md',
-  'packages/**/dist/**',
-  'packages/**/docs/**',
-  'node_modules/**',
-  'packages/**/node_modules/**',
-  'package-lock.json',
-  '.yarnrc.yml'
+  "**/CHANGELOG.md",
+  "packages/**/dist/**",
+  "packages/**/docs/**",
+  "node_modules/**",
+  "packages/**/node_modules/**",
+  "package-lock.json",
+  ".yarnrc.yml",
 ];
-const partialGenerated = ['cspell.json', 'lerna.json'];
+const partialGenerated = ["cspell.json", "lerna.json"];
 
-const esFiles = ['*.cjs', '*.ts', 'packages/**/src/**/*.ts', 'packages/**/src/**/*.mts'];
-const markdownFiles = ['*.md', 'packages/**/*.md'];
-const jsonFiles = ['*.json', 'packages/**/*.json'];
-const yamlFiles = ['.circleci/config.yml'];
-const prettyFiles = [].concat(esFiles).concat(markdownFiles).concat(jsonFiles).concat(yamlFiles);
-const spellingFiles = [].concat(esFiles).concat(markdownFiles).concat(jsonFiles).concat(yamlFiles);
+const esFiles = [
+  "*.cjs",
+  "*.ts",
+  "packages/**/src/**/*.ts",
+  "packages/**/src/**/*.mts",
+];
+const markdownFiles = ["*.md", "packages/**/*.md"];
+const jsonFiles = ["*.json", "packages/**/*.json"];
+const yamlFiles = [".circleci/config.yml"];
+const prettyFiles = []
+  .concat(esFiles)
+  .concat(markdownFiles)
+  .concat(jsonFiles)
+  .concat(yamlFiles);
+const spellingFiles = []
+  .concat(esFiles)
+  .concat(markdownFiles)
+  .concat(jsonFiles)
+  .concat(yamlFiles);
 
 const esFilesExclude = generated;
 const markdownFilesExclude = generated;
@@ -35,5 +48,5 @@ export default {
   prettyFiles,
   prettyFilesExclude,
   spellingFiles,
-  spellingFilesExclude
+  spellingFilesExclude,
 };
